af1dcb1f386e6fc40d0b3a69cbb594a162b51854
Peter Saintal <peterwsaintal@gmail.com>
2023-08-06 18:21:42 -0500

    Footer

--- script.js
+++ script.js
@@ -6,3 +6,28 @@ function myFunction() {
       x.style.display = "block";
     }
   }
+
+ 
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function getHelp() {
+  document.getElementById("getHelpMenu").classList.toggle("show");
+}
+
+function aboutNike() {
+  document.getElementById("aboutNikeMenu").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+// window.onclick = function(event) {
+//   if (!event.target.matches('.dropbtn')) {
+//     var dropdowns = document.getElementsByClassName("dropdown-content");
+//     var i;
+//     for (i = 0; i < dropdowns.length; i++) {
+//       var openDropdown = dropdowns[i];
+//       if (openDropdown.classList.contains('show')) {
+//         openDropdown.classList.remove('show');
+//       }
+//     }
+//   }
+// }
